29d8f8ee1b60ea7a0a369a04b79a2e83e1f02a4d
antonNyukhin <antonibrooke@gmail.com>
2023-10-18 19:49:47 +0300

    доделал 2 часть задания

--- js/script.js
+++ js/script.js
@@ -10,23 +10,60 @@ const personalMovieDB = {
 let filmName;
 let filmRating;
 
-for (let i = 0; i < personalMovieDB.count; i++) {
+//Способ 1
+
+for (let i = 0; i < personalMovieDB.count;) {
     filmName = prompt("Один из последних просмотренных фильмов", '');
-    filmRating = prompt("На сколько оцените его?", '');
-    if (filmName === '' || filmName === null || filmName.length > 50) {
+    if (filmName == '' || filmName == null || filmName.length > 50) {
         continue;
     } else {
-        personalMovieDB.movies[filmName] = filmRating;
+        filmRating = prompt("На сколько оцените его?", '');
+        if (filmRating == '' || filmRating == null || filmRating .length > 50) {
+            continue;
+        } 
+    personalMovieDB.movies[filmName] = filmRating;
+    i++;
     };
 };
+
+//Способ 2
+
+// let i = 0;
+// while (i < personalMovieDB.count) {
+//     filmName = prompt("Один из последних просмотренных фильмов", '');
+//     if (filmName === '' || filmName === null || filmName.length > 50) {
+//         continue;
+//     } else {
+//         filmRating = prompt("На сколько оцените его?", '');
+//         personalMovieDB.movies[filmName] = filmRating;
+//     };
+//     i++;
+// }
+
+//Способ 3
+
+// let i = 0;
+// do {
+//     filmName = prompt("Один из последних просмотренных фильмов", '');
+//     if (filmName === '' || filmName === null || filmName.length > 50) {
+//         continue;
+//     } else {
+//         filmRating = prompt("На сколько оцените его?", '');
+//         personalMovieDB.movies[filmName] = filmRating;
+//     };
+//     i++;
+// }
+// while (i < personalMovieDB.count);
+
 
 if (personalMovieDB.count <= 10) {
     alert("Просмотрено довольно мало фильмов");
 } else if (personalMovieDB.count < 30 && personalMovieDB.count > 10) {
-    alert("Выклассический зритель");
+    alert("Вы классический зритель");
 } else if (personalMovieDB.count > 30) {
     alert("Вы киноман");
 } else {
     alert("Произошла ошибка");
 };
+
 
