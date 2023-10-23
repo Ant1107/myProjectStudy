d51c0f3cfde22b84442a4c56d6b16face5fc5d04
Anton Niyhin <a.niyhin@geopack-corp.geopack.ru>
2023-10-23 13:01:15 +0300

    Educate a array theme

--- js/run.js
+++ js/run.js
@@ -1,7 +1,16 @@
-const arr = [1, 2, 3, 4, 5, 6, 6, 7, 8, 9];
-arr.pop();
-arr.push(100);
-console.log(arr);
-arr[20] = 100;
-console.log(arr);
-console.log(arr.length);
+const arr = [1,2,3,54,55,66,77,21,34,56,67];
+console.log(arr.sort((a,b) => b-a));
+//метод forEach - метод перебора,
+//метод map, reduce, filter, map - методы модификаторы,
+//методы pop, push, shift, unshift, split, join
+//псевдомассивы - массивы не имеющие методов.
+
+const arr2 = [1,2,3,54,55,66,77,21,34,56,67];
+
+const obj = new Object();
+
+for (let i = 0; i < arr.length; i++) {
+    obj[arr[i]] = arr2[i];
+}
+
+console.log(obj);
