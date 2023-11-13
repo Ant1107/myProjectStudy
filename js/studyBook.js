1ee22ace69df4048e330e46ab3c93685a6e3de29
Anton Niyhin <a.niyhin@geopack-corp.geopack.ru>
2023-11-13 15:19:57 +0300

    updated

---
+++ js/studyBook.js
@@ -0,0 +1,40 @@
+// //Глава 2
+// console.log("\nExample 1\n");
+
+// let myStr = '';
+// while (myStr.length < 7) {
+//     myStr = myStr + '#';
+//     console.log(myStr);
+// }
+
+// console.log("\nExample 2\n");
+
+// for (let i = 1; i < 100; i++) {
+//     if (i % 3 == 0) {
+//         console.log("Fizz");
+//     }
+//     else if (i % 5 == 0 && i % 3 != 0) {
+//         console.log("Buzz");
+//     }
+//     else {
+//         console.log(i);
+//     }
+// }
+
+// console.log("\nExample 3\n");
+
+// //let xx = prompt("Количество символов по оси x");
+// //let yy = prompt("Количество символов по оси y");
+// let str_y = ''
+// for (let x = 0; x < 100; x++) {
+//     for (let y = 0; y < 100; y++){
+//         if (y%2==x%2) {
+//             str_y = str_y + ' '
+//         }  else {
+//             str_y = str_y + '# '
+//         }
+//     }
+//     console.log(str_y);
+//     str_y = '';
+// }
+
